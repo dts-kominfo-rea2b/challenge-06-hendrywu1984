32f5f323dd50c40fd84919de87a498e70232dcb5
Hendry Gunawan <hendry_wu1984@yahoo.com>
2022-06-12 22:26:50 +0700

    Hendry Gunawan 152235865101-334 done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,25 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const result = [];
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) fnCallback(err, null);
+    result.push(JSON.parse(data).message.split(" ")[1]);
+  
+    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+      if (err) fnCallback(err, null);
+      result.push(JSON.parse(data)[0].message.split(' ')[1]);
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+        if (err) fnCallback(err, null);
+        result.push(JSON.parse(data)[0].data.message.split(" ")[1]);
+
+        fnCallback(null, result);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
